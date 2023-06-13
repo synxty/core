a0faefd43e5bb8ed795c88f2cd75b73556eb8c36
Pedro Resende <ceo.synxty@gmail.com>
2023-06-13 21:40:44 +0100

    fix types trailing commas

--- callisto/design-tokens/src/types/colors.ts
+++ callisto/design-tokens/src/types/colors.ts
@@ -27,52 +27,52 @@ export type Color = {
 export type NeutralColor = {
   DEFAULT: string;
   tint: {
-    10: string,
-    20: string,
-    30: string,
-    40: string,
-    50: string,
-    60: string,
-    70: string,
-    80: string,
-    90: string,
-    100: string,
-    110: string,
-    120: string
+    10: string;
+    20: string;
+    30: string;
+    40: string;
+    50: string;
+    60: string;
+    70: string;
+    80: string;
+    90: string;
+    100: string;
+    110: string;
+    120: string;
   };
   shade: {
-    10: string,
-    20: string,
-    30: string,
-    40: string,
-    50: string,
-    60: string,
-    70: string,
-    80: string,
-    90: string,
-    100: string,
-    110: string,
-    120: string
+    10: string;
+    20: string;
+    30: string;
+    40: string;
+    50: string;
+    60: string;
+    70: string;
+    80: string;
+    90: string;
+    100: string;
+    110: string;
+    120: string;
   };
 }
 
 export type Aliases = {
-  primary: Color,
-  secondary: Color,
-  success: Color,
-  info: Color,
-  warning: Color,
+  primary: Color;
+  secondary: Color;
+  success: Color;
+  info: Color;
+  warning: Color;
   danger: Color
-  red: Color,
-  vermilion: Color,
-  orange: Color,
-  amber: Color,
-  yellow: Color,
-  chartreuse: Color,
-  green: Color,
-  teal: Color,
+  red: Color;
+  vermilion: Color;
+  orange: Color;
+  amber: Color;
+  yellow: Color;
+  chartreuse: Color;
+  green: Color;
+  teal: Color;
   blue: Color
-  violet: Color,
-  purple: Color,
-  magenta: Color
+  violet: Color;
+  purple: Color;
+  magenta: Color;
 }
