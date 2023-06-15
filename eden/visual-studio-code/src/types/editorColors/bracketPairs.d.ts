c1585e17cc93d21dea2086b0d7a0487574adeead
Pedro Resende <ceo.synxty@gmail.com>
2023-06-16 00:51:03 +0100

    Eden(visual-studio-code): Add Brackets colors  (#64)

---
+++ eden/visual-studio-code/src/types/editorColors/bracketPairs.d.ts
@@ -0,0 +1,25 @@
+const bracketPairsKeys = [
+  "editorBracketMatch.background",
+  "editorBracketMatch.border",
+  "editorBracketHighlight.foreground1",
+  "editorBracketHighlight.foreground2",
+  "editorBracketHighlight.foreground3",
+  "editorBracketHighlight.foreground4",
+  "editorBracketHighlight.foreground5",
+  "editorBracketHighlight.foreground6",
+  "editorBracketHighlight.unexpectedBracket.foreground",
+  "editorBracketPairGuide.activeBackground1",
+  "editorBracketPairGuide.activeBackground2",
+  "editorBracketPairGuide.activeBackground3",
+  "editorBracketPairGuide.activeBackground4",
+  "editorBracketPairGuide.activeBackground5",
+  "editorBracketPairGuide.activeBackground6",
+  "editorBracketPairGuide.background1",
+  "editorBracketPairGuide.background2",
+  "editorBracketPairGuide.background3",
+  "editorBracketPairGuide.background4",
+  "editorBracketPairGuide.background5",
+  "editorBracketPairGuide.background6",
+] as const;
+
+export type BracketPairs = Partial<{ [K in typeof bracketPairsKeys[number]]: string }>;
