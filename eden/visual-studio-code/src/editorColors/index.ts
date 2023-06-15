0b11d3a39d1b94ec94a4c1291e1c13c07d23b54e
Pedro Resende <ceo.synxty@gmail.com>
2023-06-16 00:56:15 +0100

    update editor background color

--- eden/visual-studio-code/src/editorColors/index.ts
+++ eden/visual-studio-code/src/editorColors/index.ts
@@ -1,5 +1,14 @@
+import { EditorColors } from '../types';
+import { bracketPairs } from './bracketPairs';
+import { codeLens } from './codeLens';
 import { general } from './general';
+import { lightbulb } from './lightbulb';
+import { overviewRuler } from './overviewRuler';
 
-export const editorColors = {
-  ...general
+export const editorColors: EditorColors = {
+  ...general,
+  ...codeLens,
+  ...lightbulb,
+  ...bracketPairs,
+  ...overviewRuler
 }
--- eden/visual-studio-code/src/types/editorColors/index.d.ts
+++ eden/visual-studio-code/src/types/editorColors/index.d.ts
@@ -1,1 +1,22 @@
-export * from './general';
+import { CodeLens } from './codeLens';
+export { CodeLens } from './codeLens';
+
+import { General } from './general';
+export { General } from './general';
+
+import { LightBulb } from './lightbulb';
+export { LightBulb } from './lightbulb';
+
+import { BracketPairs } from './bracketPairs';
+export { BracketPairs } from './bracketPairs';
+
+import { OverviewRuler } from './overviewRuler';
+export { OverviewRuler } from './overviewRuler';
+
+export type EditorColors = 
+  General | 
+  CodeLens | 
+  LightBulb |
+  BracketPairs |
+  OverviewRuler
+;
