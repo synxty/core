1108de83ff8b290da5535770415b9698792f40a3
Pedro Resende <ceo.synxty@gmail.com>
2023-06-15 11:01:44 +0100

    Eden: Add release script (#59)

---
+++ eden/visual-studio-code/scripts/ajust-package-name.ts
@@ -0,0 +1,17 @@
+import { writeFileSync } from 'node:fs';
+import pkg from '../package.json';
+
+if (process.argv.length !== 3) {
+  console.error('Please provide only one argument.');
+  process.exit(1);
+}
+
+const [ PACKAGE_NAME ]: string[] = process.argv.slice(2);
+
+const result = {
+  ...pkg,
+  name: PACKAGE_NAME
+}
+
+writeFileSync(`./package.json`, JSON.stringify(result, null, 2));
+console.log('Package name changed to', PACKAGE_NAME);
--- eden/visual-studio-code/scripts/generate-marketplace-name.ts
+++
@@ -1,9 +0,0 @@
-import { writeFileSync } from 'node:fs';
-import pkg from '../package.json';
-
-const result = {
-  ...pkg,
-  name: 'eden-vscode'
-}
-
-writeFileSync(`./package.json`, JSON.stringify(result, null, 2));
