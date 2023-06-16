f2b1ddfad2fd691aa89daa2d1ba295b574c2830d
Pedro Resende <ceo.synxty@gmail.com>
2023-06-17 00:53:23 +0100

    add window border colors

--- eden/visual-studio-code/src/colors/index.ts
+++ eden/visual-studio-code/src/colors/index.ts
@@ -1,9 +1,11 @@
 import { activityBar } from './activityBar';
 import { editorColors } from './editorColors';
 import { sideBar } from './sideBar';
+import { windowBorder } from './windowBorder';
 
 export default {
   ...activityBar,
   ...editorColors,
   ...sideBar,
+  ...windowBorder
 }
---
+++ eden/visual-studio-code/src/colors/windowBorder/index.ts
@@ -0,0 +1,9 @@
+import { colors } from '@synxty/design-tokens';
+import {  WindowBorder } from '../../types';
+
+const { black } = colors;
+
+export const windowBorder: WindowBorder = {
+ "window.activeBorder": black.tint[20],
+ "window.inactiveBorder": black.tint[20]
+};
