68a71729bd803bb98290486f96da577e0c9d3020
Pedro Resende <ceo.synxty@gmail.com>
2023-06-18 00:08:10 +0100

    fix side bar and title bar borders

--- eden/visual-studio-code/src/colors/sideBar/index.ts
+++ eden/visual-studio-code/src/colors/sideBar/index.ts
@@ -7,7 +7,7 @@ const { black, white } = colors;
 export const sideBar: SideBar = {
   "sideBar.background": black.tint[30],
   "sideBar.foreground": white.shade[10],
-  "sideBar.border": black.tint[20],
+  "sideBar.border": black.tint[30],
   "sideBar.dropBackground": TESTING,
   "sideBarTitle.foreground": white.shade[10],
   "sideBarSectionHeader.background": black.tint[30],
