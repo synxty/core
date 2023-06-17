7378b78e700a516173857bd4e1ec70690dbf9c75
Pedro Resende <ceo.synxty@gmail.com>
2023-06-17 21:50:44 +0100

    Eden(visual-studio-code): Fix pack script (#82)

--- eden/visual-studio-code/scripts/pack.sh
+++ eden/visual-studio-code/scripts/pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '> Generating Theme Icon...'
-pnpm tsx src/utils/generateIcon.ts
+pnpm tsx src/utils/generateThemeIcon.ts
 
 echo '> Generating VS Code Marketplace package name..'
 pnpm tsx src/utils/changePackageName.ts 'eden-vscode'
