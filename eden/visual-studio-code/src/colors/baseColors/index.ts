12081ae9060da0944ffa914f1bd03d0e3b22e3cd
Pedro Resende <ceo.synxty@gmail.com>
2023-06-18 21:28:54 +0100

    add base colors

---
+++ eden/visual-studio-code/src/colors/baseColors/index.ts
@@ -0,0 +1,19 @@
+import { aliases, colors } from '@synxty/design-tokens';
+import { TESTING } from '../../constants';
+import { BaseColors } from '../../types';
+
+const { black, white } = colors;
+const { primary } = aliases;
+
+export const baseColors: BaseColors = {
+  "focusBorder": black.tint[90],
+  "foreground": white.shade[10],
+  "disabledForeground": black.tint[90],
+  "widget.border": black.DEFAULT,
+  "widget.shadow": `${black.DEFAULT}40`,
+  "selection.background": primary.shade[40],
+  "descriptionForeground": white.shade[120],
+  "errorForeground": TESTING,
+  "icon.foreground": black.tint[120],
+  "sash.hoverBorder": TESTING
+};
---
+++ eden/visual-studio-code/src/types/BaseColors/index.ts
@@ -0,0 +1,16 @@
+import { ParseThemeColors } from '..';
+
+const baseColorsProps = [
+  "focusBorder",
+  "foreground",
+  "disabledForeground",
+  "widget.border",
+  "widget.shadow",
+  "selection.background",
+  "descriptionForeground",
+  "errorForeground",
+  "icon.foreground",
+  "sash.hoverBorder"
+] as const;
+
+export type BaseColors = ParseThemeColors<typeof baseColorsProps>
