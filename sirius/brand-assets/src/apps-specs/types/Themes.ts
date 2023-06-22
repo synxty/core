a01f8b8dc4563569543866f7e687776352dd6f5e
Pedro Resende <ceo.synxty@gmail.com>
2023-06-22 20:42:45 +0100

    fix filenames in brand-assets types

---
+++ sirius/brand-assets/src/apps-specs/types/Themes.ts
@@ -0,0 +1,11 @@
+import { supportedThemes } from 'apps-specs/themes/index.ts';
+
+export type SupportedThemes = typeof supportedThemes[keyof typeof supportedThemes];
+
+export type ThemeConfig = {
+  backgroundColor: string;
+}
+
+export type Themes = {
+  [key in SupportedThemes]: ThemeConfig
+};
