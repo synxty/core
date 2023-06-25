0d659d725b897eb9acb7b800d4104f097982e10f
Pedro Resende <ceo.synxty@gmail.com>
2023-06-25 13:47:12 +0100

    add eslint to eden visual-studio-code and fix linting errors

--- eden/visual-studio-code/src/utils/getObjectDifferences.ts
+++ eden/visual-studio-code/src/utils/getObjectDifferences.ts
@@ -3,14 +3,14 @@ export function getObjectDifferences<T extends object>(obj1: T, obj2: T): Partia
   const differences: Partial<T> = {};
 
   for (const key of keys) {
-    const value1 = (obj1 as any)[key as keyof T];
-    const value2 = (obj2 as any)[key as keyof T];
+    const value1 = obj1[key as keyof T];
+    const value2 = obj2[key as keyof T];
 
     if (value1 !== value2) {
       if (typeof value1 === 'object' && typeof value2 === 'object') {
         const nestedDifferences = getObjectDifferences(value1, value2);
         if (nestedDifferences !== null && Object.keys(nestedDifferences).length > 0) {
-          (differences as any)[key as keyof T] = nestedDifferences;
+          differences[key as keyof T] = nestedDifferences as T[keyof T];
         }
       } else {
         differences[key as keyof T] = value2;
