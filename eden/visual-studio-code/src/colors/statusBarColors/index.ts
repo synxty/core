f25a20e74c2aa037a6c58dbc596897c5e6d95963
Pedro Resende <ceo.synxty@gmail.com>
2023-07-01 23:21:29 +0100

    fix status bar colors

--- eden/visual-studio-code/src/colors/statusBarColors/index.ts
+++ eden/visual-studio-code/src/colors/statusBarColors/index.ts
@@ -1,9 +1,9 @@
 import { aliases, colors } from '@synxty/design-tokens';
-import { TESTING } from 'constants/index.ts';
+import { DEBUGGING, TESTING } from 'constants/index.ts';
 import { StatusBarColors } from 'types/index.ts';
 
 const { black, white } = colors;
-const { primary, danger, warning } = aliases;
+const { primary, danger, warning, amber, secondary } = aliases;
 
 export const statusBarColors: StatusBarColors = {
   'statusBar.background': black.tint[20],
@@ -15,18 +15,18 @@ export const statusBarColors: StatusBarColors = {
   'statusBar.noFolderBackground': primary.DEFAULT,
   'statusBar.noFolderForeground': black.tint[30],
   //"statusBar.noFolderBorder": TESTING,
-  //"statusBarItem.activeBackground": TESTING,
-  //"statusBarItem.hoverBackground": TESTING,
+  'statusBarItem.activeBackground': `${black.tint[70]}40`,
+  'statusBarItem.hoverBackground': `${black.tint[110]}40`,
   'statusBarItem.prominentForeground': TESTING,
-  'statusBarItem.prominentBackground': TESTING,
+  'statusBarItem.prominentBackground': black.tint[70],
   'statusBarItem.prominentHoverBackground': TESTING,
   'statusBarItem.remoteBackground': black.tint[20],
-  'statusBarItem.remoteForeground': primary.DEFAULT,
+  'statusBarItem.remoteForeground': amber.DEFAULT,
   'statusBarItem.errorBackground': danger.DEFAULT,
   'statusBarItem.errorForeground': white.shade[10],
   'statusBarItem.warningForeground': white.shade[10],
   'statusBarItem.warningBackground': warning.DEFAULT,
-  'statusBarItem.compactHoverBackground': TESTING,
+  'statusBarItem.compactHoverBackground': `${black.tint[110]}40`,
   'statusBarItem.focusBorder': TESTING,
   'statusBar.focusBorder': black.tint[70],
   'statusBar.offlineBackground': TESTING,
